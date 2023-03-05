206c370bddecd99eec659246667396ddc1f2719c
Felipe Pérez Leal <62476135+felipeperezleal@users.noreply.github.com>
2023-03-05 15:40:16 -0500

    Update lab0db.sql

--- lab0db.sql
+++ lab0db.sql
@@ -29,7 +29,7 @@ CREATE TABLE `municipio` (
   `presupuesto` int DEFAULT NULL,
   `gobernador` int NOT NULL,
   PRIMARY KEY (`id_municipio`),
-  UNIQUE KEY `governador_UNIQUE` (`gobernador`)
+  UNIQUE KEY `gobernador_UNIQUE` (`gobernador`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
